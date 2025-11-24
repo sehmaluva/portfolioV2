34d1117f5d73abc77d862adadc0988016a681b43
sehmaluva <machinguramalvint@gmail.com>
2025-11-24 08:19:35 +0200

    feat: add SpeedInsights component to layout for performance tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -86,6 +86,7 @@ export default function RootLayout({
         <MatrixBackground />
         <Suspense>
           {children}
+          <SpeedInsights />
           <Analytics />
         </Suspense>
       </body>
